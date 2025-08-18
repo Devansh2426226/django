1e6b2da2a4cf72c6ef170659a2f4a315b115dd88
Devansh2426226 <devanshpandey2604@gmail.com>
2025-08-18 20:56:12 +0530

    added comment

--- my_website/django_env/django_env/urls.py
+++ my_website/django_env/django_env/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include # python manage.py runserver
 from .views import index
 from .views import home
 from .views import index
